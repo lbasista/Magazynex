adae5415680b2f2c94c33e0a04115e40873dfa6a
Łukasz Basista <lukasz.basista.2001@gmail.com>
2025-05-16 12:27:34 +0200

    Fixed updating products. Since now, user don't need to return to list of products, to refresh new product data

--- app/src/main/java/pl/lbasista/magazynex/ui/product/ProductDetailsActivity.java
+++ app/src/main/java/pl/lbasista/magazynex/ui/product/ProductDetailsActivity.java
@@ -11,16 +11,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import pl.lbasista.magazynex.R;
 
 public class ProductDetailsActivity extends AppCompatActivity {
+    private TextView textProductName, textProducer, textBarcode, textQuantity, textDescription;
+    private ImageView imageViewProduct;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
+
+        //Inicjalizacja pól widoków
+        textProductName = findViewById(R.id.textProductName);
+        textProducer = findViewById(R.id.textProducer);
+        textBarcode = findViewById(R.id.textBarcode);
+        textQuantity = findViewById(R.id.textQuantity);
+        textDescription = findViewById(R.id.textDescription);
+        imageViewProduct = findViewById(R.id.imageProduct);
 
         //Powrót do listy produktów
         findViewById(R.id.tvBack).setOnClickListener(v -> finish());
 
         //Edycja
         findViewById(R.id.tvEdit).setOnClickListener(v -> {
+            //Dane z Intent
             Bundle args = new Bundle();
             args.putString("barcode", getIntent().getStringExtra("barcode"));
             args.putString("name", getIntent().getStringExtra("name"));
@@ -31,30 +43,40 @@ public class ProductDetailsActivity extends AppCompatActivity {
 
             EditProductBottomSheet sheet = new EditProductBottomSheet();
             sheet.setArguments(args);
+
+            //Aktualizowanie obiektu po edycji
+            sheet.setOnProductUpdatedListener(updatedProduct -> {
+                //Nowe wartości
+                textProductName.setText(updatedProduct.name);
+                textProducer.setText(updatedProduct.producer);
+                textBarcode.setText(updatedProduct.barcode);
+                textQuantity.setText(String.valueOf(updatedProduct.quantity));
+                textDescription.setText(updatedProduct.description);
+                if (!updatedProduct.imageUri.isEmpty()) {
+                    imageViewProduct.setImageURI(Uri.parse(updatedProduct.imageUri));
+                    imageViewProduct.setVisibility(View.VISIBLE);
+                } else {
+                    imageViewProduct.setVisibility(View.GONE);
+                }
+            });
             sheet.show(getSupportFragmentManager(), "editProduct");
         });
+        loadFromIntent();
+    }
 
-
+    private void loadFromIntent() {
         //Pobranie danych
-        String name = getIntent().getStringExtra("name");
-        String producer = getIntent().getStringExtra("producer");
-        String barcode = getIntent().getStringExtra("barcode");
-        int quantity = getIntent().getIntExtra("quantity", 0);
-        String description = getIntent().getStringExtra("description");
-        String imageUri = getIntent().getStringExtra("imageUri");
-
-        //Ustawienie danych
-        ((TextView)findViewById(R.id.textProductName)).setText(name);
-        ((TextView)findViewById(R.id.textProducer)).setText(producer);
-        ((TextView)findViewById(R.id.textBarcode)).setText(barcode);
-        ((TextView)findViewById(R.id.textQuantity)).setText(String.valueOf(quantity));
-        ((TextView)findViewById(R.id.textDescription)).setText(description);
-
-        ImageView imageView = findViewById(R.id.imageProduct);
-        if (imageView != null && !imageUri.isEmpty()) {
-            imageView.setImageURI(Uri.parse(imageUri));
+        textProductName.setText(getIntent().getStringExtra("name"));
+        textProducer.setText(getIntent().getStringExtra("producer"));
+        textBarcode.setText(getIntent().getStringExtra("barcode"));
+        textQuantity.setText(String.valueOf(getIntent().getIntExtra("quantity", 0)));
+        textDescription.setText(getIntent().getStringExtra("description"));
+        String uri = getIntent().getStringExtra("imageUri");
+        if (uri != null && !uri.isEmpty()) {
+            imageViewProduct.setImageURI(Uri.parse(uri));
+            imageViewProduct.setVisibility(View.VISIBLE);
         } else {
-            imageView.setVisibility(TextView.GONE);
+            imageViewProduct.setVisibility(View.GONE);
         }
     }
 }
